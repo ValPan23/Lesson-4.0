6da76be440896bc50c378af7278de5c80ca59cb0
ValPan23 <164073142+ValPan23@users.noreply.github.com>
2024-04-17 01:55:49 +0300

    new code in main_1

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-# Функция `calculate` для вычисления суммы двух чисел
-def calculate(a, b):
+# Калькулятор сложения
+def add_numbers(a, b):
     return a + b
 
+if __name__ == "__main__":
+    num1 = float(input("Введите первое число: "))
+    num2 = float(input("Введите второе число: "))
+    result = add_numbers(num1, num2)
+    print(f"Результат сложения: {result}")
 
-result = calculate(5, 3)
-print('Сумма:', result)
 
