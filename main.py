d9f750f543052bb726f0497ad405d26f494ae3f2
ValPan23 <164073142+ValPan23@users.noreply.github.com>
2024-04-17 03:55:03 +0300

    new code in main

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 
+# Определяем переменные a и b
+a = 5
+b = 3
 # Функция calculate_sum, вычисляющая сумму
 def calculate_sum(a, b):
-
     return a + b
 
+# передаём аргументы функции и сохраняем результат в переменную
+result_sum = calculate_sum(a, b)
 
-result_sum = calculate_sum(5, 3)
+# Выводим результат
 print('Сумма:', result_sum)
 
 # Функция calculate_product, вычисляющая произведение
 def calculate_product(a, b):
-
     return a * b
 
+# передаём аргументы функции и сохраняем результат в переменную
+result_product = calculate_product(a, b)
 
-result_product = calculate_product(5, 3)
+# Выводим результат
 print('Произведение:', result_product)
+
